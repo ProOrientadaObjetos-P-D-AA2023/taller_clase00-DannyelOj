c8c6a6814c3c5582cf14ad51c4eba5cd0cdfff5f
Daniel Ojeda <dmojeda@utpl.edu.ec>
2023-04-19 10:32:21 -0500

    Subiendo Taller

---
+++ Deber1-Daniel Ojeda/src/deber1/daniel/ojeda/Deber1DanielOjeda.java
@@ -0,0 +1,16 @@
+package deber1.daniel.ojeda;
+
+/**
+ *
+ * @author VEL-USER
+ */
+public class Deber1DanielOjeda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
